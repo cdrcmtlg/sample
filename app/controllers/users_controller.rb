7348a74021c7d2da24ca52fa815bc235fb462535
Cedric Matalog <cedric.matalog@adish.co.jp>
2016-05-11 07:03:30 +0000

    Finish log in / log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
     # @user = User.new(params[:user])
     @user = User.new(user_params)
     if @user.save
+      log_in  @user
       flash[:success] = "Welcome to  TWOOTOR!"
+      
       redirect_to @user
     else
       render 'new'
